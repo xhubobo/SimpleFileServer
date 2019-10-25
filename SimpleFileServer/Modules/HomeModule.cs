92c6c23f3ad8ebf85729705ae3f9fb39f2e143e2
zhangbo <19bobo@sina.com>
2019-10-25 11:56:06 +0800

    * 修改主页信息

--- SimpleFileServer/Modules/HomeModule.cs
+++ SimpleFileServer/Modules/HomeModule.cs
@@ -7,20 +7,15 @@ namespace SimpleFileServer.Modules
     {
         public HomeModule()
         {
-            Get["/"] = r =>
-            {
-                var os = Environment.OSVersion;
-                return "SimpleFileServer implemented by Nancy<br/> System:" + os.VersionString;
-            };
-            // 
-            //             Get["/mvc/{controller}/{action}/{id}"] = r =>
-            //             {
-            //                 var mvc = new StringBuilder();
-            //                 mvc.AppendLine("controller :" + r.controller + "<br/>");
-            //                 mvc.AppendLine("action :" + r.action + "<br/>");
-            //                 mvc.AppendLine("id :" + r.id + "<br/>");
-            //                 return mvc.ToString();
-            //             };
+            Get["/"] = OnMainPage;
+        }
+
+        private Response OnMainPage(dynamic o)
+        {
+            var info = $"SimpleFileServer implemented by Nancy.{Environment.NewLine}" +
+                       $"MachineName: {Environment.MachineName}.{Environment.NewLine}" +
+                       $"System: {Environment.OSVersion.VersionString}.";
+            return Response.AsText(info);
         }
     }
 }
